a5b7cc3d4e4bcaeb0d91b047b26ee2be94be0190
Charlotte Ennis <charlotteennis@Charlottes-MacBook-Pro.local>
2025-08-19 09:19:55 -0400

    double check date format test

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -2,8 +2,8 @@ import { formatDate } from "./formatDate";
 
 describe("utils tests", () => {
     it("formats a date correctly", () => {
-        const dateString = "2023-10-01T12:00:00Z";
+        const dateString = "2025-08-18T12:00:00Z";
         const formattedDate = formatDate(dateString);
-        expect(formattedDate).toBe("October 1, 2023");
+        expect(formattedDate).toBe("August 18, 2025");
     });
 })
